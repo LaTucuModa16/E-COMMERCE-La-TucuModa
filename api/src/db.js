5d216d198a2b510eb0ba99647a3a7751df370b67
LucianoVilla95 <88258891+LucianoVilla95@users.noreply.github.com>
2023-02-01 20:53:54 -0300

    modelo de categorias y ruta

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { } = sequelize.models;
+const { Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-// Pokemon.belongsToMany(Type, {through : 'pokemons_types'});
-// Type.belongsToMany(Pokemon, {through : 'pokemons_types'});
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
