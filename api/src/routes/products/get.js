1b681a677f722bf18f430bed3144ede9f273c973
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2023-02-01 20:52:53 -0300

    Create Product (#10)
    * Create Product
    
    * Update Product.js
    
    * Product completed
    
    * Created Model Product
    * Created Controllers Product
    * Created Routes Product ( Get, Post, Delete)

---
+++ api/src/routes/products/get.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const { getProduct, getProducts } = require("../../controllers/products");
+const { Product, Categorie } = require("../../db");
+
+router.get("/", async (req, res) => {
+    const { name } = req.body;
+    try {
+        if (name) {
+            const product = await getProduct(name);
+            res.send(product);
+        } else {
+            const all = await getProducts();
+            res.send(all);
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const all = await getProducts();
+
+    const product = await all.filter(e => e.id == id)
+    if (product.length) {
+        res.status(200).send(product)
+    } else {
+        res.status(404).send("Id incorrecto")
+    }
+})
