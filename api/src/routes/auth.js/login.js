a3d4c4998ccb472f04ed3aeaab1995fef07ecf43
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2023-02-03 16:26:06 -0300

    Update Login

--- api/src/routes/auth.js/login.js
+++ api/src/routes/auth.js/login.js
@@ -1,43 +1,8 @@
 const router = require("express").Router();
-const { User } = require("../../db")
-const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken");
+const {User} = require ("../../db");
 
-//LOGIN
-router.post("/", async (req, res) => {
-    try {
-        const { username } = req.body;
-
-        const user = await User.findOne({ username })
-        if (!user) {
-            res.status(401).json("This user does not exist")
-        }
-
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-
-        const pass = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        if (pass !== req.body.password) {
-            res.status(401).json("The credentials are wrong")
-        }
-
-        const accessToken = jwt.sign({
-            id: user.id
-        },
-            process.env.JWT_SEC,
-            { expiresIn: "3d" }
-        )
-
-
-        res.status(200).json({ user, token: accessToken })
-    } catch (error) {
-        res.status(404).json(error)
-    }
-
+router.post("/", async (req, res)=> {
+    const {email, password} = req.body;
 })
-
 
 module.exports = router;
