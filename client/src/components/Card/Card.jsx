01884ba4fc0b07056c4ea30019435926f82b459c
Pato Barbero <patogbarbero@gmail.com>
2023-02-03 18:58:00 -0300

    Paginado & Detail & Algo css

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 // import "./card.css";
+import { Card, Button } from "react-bootstrap";
 
-export default function Card({ name, img, price }) {
+export default function Card_({ name, img, price }) {
   return (
-    <div>
-      <h1>{name}</h1>
-      <img src={img} alt="imagen del producto" width="170px" height="220px" />
-      <p>$ {price}.-</p>
-    </div>
+    // <div>
+    //   <h1>{name}</h1>
+    //   <img src={img} alt="imagen del producto" width="170px" height="220px" />
+    //   <p>$ {price}.-</p>
+    // </div>
+    <Card className="mt-4" style={{ width: "60%" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title className="text-primary">${price}.-</Card.Title>
+        <Card.Text>{name}</Card.Text>
+        <Button variant="success">Ver detalles</Button>
+      </Card.Body>
+    </Card>
   );
 }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Card from "../Card/Card.jsx";
 import { Link } from "react-router-dom";
 import Navbar from "../NavBar/Navbar";
+import Paginado from "../Paginado/Paginado";
 
 export default function Home() {
   const allProducts = useSelector((state) => state.products);
@@ -28,15 +29,30 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="containerCards">
+    <div>
       <Navbar />
-      {currentProducts.length > 0 ? (
-        currentProducts?.map((p, pos) => {
-          return <Card key={pos} name={p.name} img={p.img} price={p.price} />;
-        })
-      ) : (
-        <h5>No se encontraron productos con esas caracteristicas</h5>
-      )}
+      <div className="text-center">
+        <Paginado
+          productsPerPage={productsPerPage}
+          allProducts={allProducts.length}
+          settingCurrentPage={settingCurrentPage}
+          currentPage={currentPage}
+        />
+        <div className="d-flex row justify-content-around">
+          {currentProducts.length > 0 ? (
+            currentProducts?.map((p, pos) => {
+              console.log(p);
+              return (
+                <div className="col-md-6 col-lg-4 d-flex justify-content-center">
+                  <Card key={pos} name={p.name} img={p.img} price={p.price} />
+                </div>
+              );
+            })
+          ) : (
+            <h5>No se encontraron productos con esas caracteristicas</h5>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
