0dc7cd82976d28bd04f088b762d28deabcb3c611
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2023-02-12 21:19:41 -0300

    Create LoginFront

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -3,77 +3,100 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { LoginUser } from "../../actions"
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import styled from "styled-components";
+import { FaGoogle, FaFacebook, FaTwitter } from "react-icons/fa"
+import { MainContainer, WelcomeText, ButtonStyled } from "./StyledLogin";
+import "./Login.css"
 
 export default function Login() {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    const showToast = () => {
+        toast("Successfully logged in")
+    };
     const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
+
     return (
-        <Formik
-            initialValues={
-                {
-                    email: '',
-                    password: ''
-                }
-            }
+        <div className="container">
+            <MainContainer>
+                <Formik
+                    initialValues={
+                        {
+                            email: '',
+                            password: ''
+                        }
+                    }
 
-            validate={(valores) => {
-                let errores = {};
+                    validate={(valores) => {
+                        let errores = {};
 
-                //Validation Email
-                if (!valores.email) {
-                    errores.email = "Please enter the email"
-                } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
-                    errores.email = "Invalid email"
-                }
+                        //Validation Email
+                        if (!valores.email) {
+                            errores.email = "*Please enter the email"
+                        } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
+                            errores.email = "*Invalid email"
+                        }
+                        //Validation Password
+                        if (!valores.password) {
+                            errores.password = "*Please enter the password"
+                        }
+                        return errores;
+                    }}
+                    onSubmit={(valores, { resetForm }) => {
+                        dispatch(LoginUser(valores))
+                        resetForm();
+                        console.log("Formulario enviado");
+                        cambiarFormularioEnviado(true);
+                        setTimeout(() => cambiarFormularioEnviado(false), 5000)
+                        /*     navigate('/home') */
 
-                //Validation 
-                return errores;
-            }}
-            onSubmit={(valores, { resetForm }) => {
-                dispatch(LoginUser(valores))
-                resetForm();
-                console.log("Formulario enviado");
-                cambiarFormularioEnviado(true);
-                setTimeout(() => cambiarFormularioEnviado(false), 5000)
-                /*     navigate('/home') */
+                    }}
+                >
+                    {({ errors, isValid, dirty }) => (
+                        <div className="containerForm">
+                            <Form >
+                                <WelcomeText>WELCOME</WelcomeText>
+                                <div className="form">
+                                    <div>
+                                        <Field
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                            placeholder="Email"
+                                            className="inputContainer"
+                                        />
+                                        <ErrorMessage name="email" component={() => (
+                                            <div className="error">{errors.email}</div>
+                                        )} />
+                                    </div>
+                                    <div className="containerPassword">
+                                        <Field
+                                            type="text"
+                                            id="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            className="inputContainer"
+                                        />
+                                        <ErrorMessage name="password" component={() => (
+                                            <div className="error">{errors.password}</div>
+                                        )} />
+                                    </div>
+                                    <div className="containerButton">
+                                        <ButtonStyled type="submit" onClick={showToast} disabled={!(isValid && dirty)} >Sing In</ButtonStyled>
+                                    </div>
+                                    <ToastContainer position="top-right" />
 
-            }}
-        >
-            {({ errors }) => (
-                <Form >
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <Field
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="matias_aguirre@gmail.com"
-                        />
-                        <ErrorMessage name="email" component={() => (
-                            <div className="error">{errors.email}</div>
-                        )} />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <Field
-                            type="text"
-                            id="password"
-                            name="password"
-                            placeholder="*******"
-                        />
-                        <ErrorMessage name="password" component={() => (
-                            <div className="error">{errors.password}</div>
-                        )} />
-                    </div>
-                    <button type="submit">Sing In</button>
-                    {formularioEnviado && <p>Formulario enviado con exito!</p>}
-                </Form>
-            )}
-        </Formik>
-
+                                </div>
+                            </Form>
+                        </div>
+                    )
+                    }
+                </Formik >
+            </MainContainer >
+        </div >
     )
 }
 
