d0e2c8738b6703c2d4a0fd5dee544f120323dcdc
Esteban Teran <esteban.teran400@gmail.com>
2023-02-17 18:31:49 -0300

    css2

--- client/src/components/DashBoard/DashBoardHome/DashBoard.jsx
+++ client/src/components/DashBoard/DashBoardHome/DashBoard.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import NavBarDash from "../NavBarDash/NavBarDash";
-import "./DashBoard.css"
-import HomeDash from "../../DashBoard/Pages/HomeDash/HomeDash"
+import "./DashBoard.css";
+import HomeDash from "../../DashBoard/Pages/HomeDash/HomeDash";
 import Sales from "../../DashBoard/Pages/Sales/Sales";
-import Clients from "../../DashBoard/Pages/Clients/Clients"
+import Clients from "../../DashBoard/Pages/Clients/Clients";
 
 export default function DashBoard() {
-
-    return (
-        <div className="containerDash">
-            <NavBarDash />
-        </div >
-    )
+  return (
+    <div className="containerDash">
+      <NavBarDash />
+      <Routes>
+        <Route path="/dash" element={<HomeDash />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/clients" element={<Clients />} />
+      </Routes>
+    </div>
+  );
 }
 
-{/* <Routes>
+{
+  /* <Routes>
 <Route path="/dash" element={<HomeDash />} />
 <Route path="/sales" element={<Sales />} />
 <Route path="/clients" element={<Clients />} />
-</Routes> */}
+</Routes> */
+}
