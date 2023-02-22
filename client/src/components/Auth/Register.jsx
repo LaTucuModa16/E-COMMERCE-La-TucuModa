93a7af14977f44f11ae7af74d01803ccb64c582e
Esteban Teran <esteban.teran400@gmail.com>
2023-02-21 21:11:56 -0300

    arreglo de css register

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,90 +1,129 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import NavBar from '../NavBar/Navbar.jsx';
-import { registerUser, getUsers } from '../../actions/index.js';
-import { useForm } from 'react-hook-form';
-import './Regsiter.css';
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import NavBar from "../NavBar/Navbar.jsx";
+import { registerUser, getUsers } from "../../actions/index.js";
+import { useForm } from "react-hook-form";
+import "./Regsiter.css";
 
 export default function Register() {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const { register, formState: { errors }, handleSubmit } = useForm();
-	const users = useSelector(state => state.users);
-	// console.log(users, 'usersss');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const users = useSelector((state) => state.users);
+  // console.log(users, 'usersss');
 
-	const allEmail = users.map(e => e.email);
-	// console.log(allEmail);
+  const allEmail = users.map((e) => e.email);
+  // console.log(allEmail);
 
-	const emailValidate = (email) => {
-		return !allEmail.includes(email);
-	};
+  const emailValidate = (email) => {
+    return !allEmail.includes(email);
+  };
 
-	const onSubmit = (data, e) => {
-		dispatch(registerUser(data));
-		alert('Usuario creado exitosamente');
-		e.target.reset();
-		navigate('/home');
-	};
+  const onSubmit = (data, e) => {
+    dispatch(registerUser(data));
+    alert("Usuario creado exitosamente");
+    e.target.reset();
+    navigate("/home");
+  };
 
-	useEffect(() => {
-		dispatch(getUsers());
-	}, [dispatch]);
-	return (
-		<div>
-			<NavBar />
-			<div className="formCont">
-				<form onSubmit={handleSubmit(onSubmit)}>
-				<h1 className="title">Registro de Usuario</h1>
-				<div className="formUsername">
-					<label className="username">NOMBRE DE USUARIO</label>
-					<input type='text' {...register('username', {
-						required: true
-					})} />
-					{errors.username?.type === 'required' && <p className="p1">Username is required</p>}
-				</div>
-				<div className="formName">
-					<label className="name">NOMBRE</label>
-					<input type='text' {...register('name', {
-						required: true
-					})} />
-					{errors.name?.type === 'required' && <p className="p2">Name is required</p>}
-				</div>
-				<div className="formLastname">
-					<label className="lastname">APELLIDO</label>
-					<input type='text' {...register('last_name', {
-						required: true
-					})} />
-					{errors.last_name?.type === 'required' && <p className="p3">Lastname is required</p>}
-				</div>
-				<div className="formEmail">
-					<label className="email">EMAIL</label>
-					<input type='email' placeholder='username@example.com' {...register('email', {
-						required: true,
-						pattern: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-						validate: emailValidate
-					})} />
-					{errors.email?.type === 'required' && <p className="p4">Email is required</p>}
-					{errors.email?.type === 'pattern' && <p className="p5">Format incorrect</p>}
-					{errors.email?.type === 'validate' && <p className="p5">Email already exists</p>}
-				</div>
-				<div className="formPassword">
-					<label className="password">CONTRASEÑA</label>
-					<input type='password' placeholder='******' {...register('password', {
-						required: true
-					})} />
-					{errors.password?.type === 'required' && <p className="p6">Password is required</p>}
-				</div>
-				<button type='submit' className="but1">REGISTRARSE</button>
-				</form>
-				<div className="login">
-				<p>¿Ya tienes una cuenta?</p>
-				<Link to='/login'>
-					<a className="inc">Inicia Sesión</a>
-				</Link>
-				</div>
-			</div>
-		</div>
-	)
-};
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+  return (
+    <div>
+      {/* <NavBar /> */}
+      <div className="fondoReg">
+        <div className="formCont">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="title">Registro de Usuario</h1>
+            <div className="formUsername">
+              <label className="username">NOMBRE DE USUARIO</label>
+              <input
+                type="text"
+                {...register("username", {
+                  required: true,
+                })}
+              />
+              {errors.username?.type === "required" && (
+                <p className="p1">Username is required</p>
+              )}
+            </div>
+            <div className="formName">
+              <label className="name">NOMBRE</label>
+              <input
+                type="text"
+                {...register("name", {
+                  required: true,
+                })}
+              />
+              {errors.name?.type === "required" && (
+                <p className="p2">Name is required</p>
+              )}
+            </div>
+            <div className="formLastname">
+              <label className="lastname">APELLIDO</label>
+              <input
+                type="text"
+                {...register("last_name", {
+                  required: true,
+                })}
+              />
+              {errors.last_name?.type === "required" && (
+                <p className="p3">Lastname is required</p>
+              )}
+            </div>
+            <div className="formEmail">
+              <label className="email">EMAIL</label>
+              <input
+                type="email"
+                placeholder="username@example.com"
+                {...register("email", {
+                  required: true,
+                  pattern:
+                    /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+                  validate: emailValidate,
+                })}
+              />
+              {errors.email?.type === "required" && (
+                <p className="p4">Email is required</p>
+              )}
+              {errors.email?.type === "pattern" && (
+                <p className="p5">Format incorrect</p>
+              )}
+              {errors.email?.type === "validate" && (
+                <p className="p5">Email already exists</p>
+              )}
+            </div>
+            <div className="formPassword">
+              <label className="password">CONTRASEÑA</label>
+              <input
+                type="password"
+                placeholder="******"
+                {...register("password", {
+                  required: true,
+                })}
+              />
+              {errors.password?.type === "required" && (
+                <p className="p6">Password is required</p>
+              )}
+            </div>
+            <button type="submit" className="but1">
+              REGISTRARSE
+            </button>
+          </form>
+          <div className="login">
+            <p>¿Ya tienes una cuenta?</p>
+            <Link to="/login">
+              <a className="inc">Inicia Sesión</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/Auth/StyledLogin.jsx
+++ client/src/components/Auth/StyledLogin.jsx
@@ -1,51 +1,48 @@
 import styled from "styled-components";
 
-
 export const MainContainer = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    height: 80vh;
-    width: 30vw;
-    background: rgb(255,255,255,0.15);
-    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
-    backdrop-filter: blur(8.5px);
-    border-radius: 10px;
-    color: white;
-    text-transform: uppercase;
-    letter-spacing: 0.3rem;
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 80vh;
+  width: 30vw;
+  background: rgb(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+  backdrop-filter: blur(8.5px);
+  border-radius: 10px;
+  color: white;
+  text-transform: uppercase;
+  letter-spacing: 0.3rem;
+`;
 
 export const WelcomeText = styled.h2`
-    margin: 3rem 0 2rem 0;
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    color: #FABA7E;
-    
-`
+  margin: 3rem 0 2rem 0;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  color: #faba7e;
+`;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    height: 20%;
-    width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 20%;
+  width: 100%;
+`;
 export const ButtonStyled = styled.button`
-color: aliceblue;
-margin-top: 30px;
-width: 25vh;
-height: 5vh;
-box-shadow: 0 1px 2px 0px #161515e8;
-border-radius: 2rem;
-border: none;
-background: #FABA7E;
-text-transform: uppercase;
-letter-spacing: 0.2rem;
-&:hover{
+  color: aliceblue;
+  margin-top: 30px;
+  width: 25vh;
+  height: 5vh;
+  box-shadow: 0 1px 2px 0px #161515e8;
+  border-radius: 2rem;
+  border: none;
+  background: #faba7e;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  &:hover {
     background: #f8b16e;
     box-shadow: 0 1px 4px 0px #ecebebe8;
-}
-`
-
+  }
+`;
